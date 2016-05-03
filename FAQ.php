9eca7dc31ab78a9224df751276a3ab8d4e1e2da5
DM <session2@START-Coding-MBP-16.local>
2016-05-03 16:08:12 -0700

    Some other stuff...doesn't really affect the code

--- FAQ.php
+++ FAQ.php
@@ -5,3 +5,25 @@
  * Date: 4/26/16
  * Time: 5:19 PM
  */
+
+?>
+
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="FAQ.css">
+</head>
+<body>
+<center>
+<h1> Frequently Asked Questions </h1>
+</center>
+<h2> Is Diabetes a disease? </h2>
+ <h2> Yes, Diabetes is a disease. It is a metabolic disease in which the body is unable to <br>
+     produce any or enough insulin causes elevated levels of glucose in the blood. </h2>
+<h2> Can someone get rid of Diabetes?</h2>
+<h2> Yes, but it's very rare to reverse the diabetes & go back to their health state. </h2>
+<h2> What causes Diabetes?</h2>
+<h2> It usually varies, it can be caused due to genetics, or a high concentration of sugar in the body </h2>
+<h2> Tips & Tricks to help YOU keep a healthy lifestyle ! </h2>
+<h2> </h2>
+</body>
+</html>
